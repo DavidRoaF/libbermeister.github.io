99e94513016344008005d7f6b143f4b1791257e6
libbermeister <libbermeister@gmail.com>
2020-11-20 21:44:37 -0600

    Added a menu dropdown for mobile screens.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("¯\\_(ツ)_/¯");
+function menuToggle() {
+    var x = document.getElementById('_navlinks');
+    if (x.className === 'navlinks') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navlinks';
+    }
+  }
